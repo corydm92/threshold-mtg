e8b02cc912801d8da19b132d9c46083f355519ea
Cory Morrissey <contact@corymorrissey.com>
2020-07-28 11:26:59 -0700

    added single result mock

--- src/redux/reducers/CardsReducer.js
+++ src/redux/reducers/CardsReducer.js
@@ -8,7 +8,7 @@ let initState = [];
 const cardsReducer = (state = initState, action) => {
   switch (action.type) {
     case FETCH_CARD_DATA_SUCCESSFUL: {
-      return [action.payload];
+      return action.payload;
     }
     case FETCH_CARD_DATA_FAILED:
       return initState;
