69f31fc965545d7394c52e589db06247cb48bd21
Cory Morrissey <contact@corymorrissey.com>
2020-08-03 12:54:09 -0700

    added specPricesSelector

--- src/selectors/cardSelector.js
+++ src/selectors/cardSelector.js
@@ -1,3 +1,16 @@
 import { createSelector } from 'reselect';
 
 const getCard = (state) => state.cardReducer;
+const getSpecPrices = (state) => state.specPricesReducer;
+const getTcgPrices = (state) => state.tcgPricesReducer;
+
+const specPricesSelector =
+  ([getCard, getSpecPrices],
+  (card, specPrices) => {
+    card.specPrices.map((specPriceId) => {
+      return {
+        // Select parts from specPrices object here
+        ...specPrices[specPriceId],
+      };
+    });
+  });
