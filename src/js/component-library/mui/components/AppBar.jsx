3a5092dc3c0535c7123a6698991520c3906134e0
Cory Morrissey <contact@corymorrissey.com>
2020-08-04 12:24:56 -0700

    added additional padding to AppBar to match body

--- src/js/component-library/mui/components/AppBar.jsx
+++ src/js/component-library/mui/components/AppBar.jsx
@@ -3,15 +3,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Container from './Container';
 // import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => {
+  console.log(theme);
   return {
     root: {
       flexGrow: 1,
       backgroundColor: theme.palette.primary.main,
+      padding: '0px 8px',
     },
     menuButton: {
       marginRight: theme.spacing(1),
@@ -29,16 +32,18 @@ export default function ButtonAppBar(props) {
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" className={classes.root}>
-        <Toolbar>
-          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <AppBar position='fixed' className={classes.root}>
+        <Container>
+          <Toolbar disableGutters={true}>
+            {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton> */}
-          <Typography variant="h6" className={classes.title}>
-            {title}
-          </Typography>
-          {/* <Button color="inherit">Login</Button> */}
-        </Toolbar>
+            <Typography variant='h6' className={classes.title}>
+              {title}
+            </Typography>
+            {/* <Button color="inherit">Login</Button> */}
+          </Toolbar>
+        </Container>
       </AppBar>
     </div>
   );
--- src/js/components/MainView.jsx
+++ src/js/components/MainView.jsx
@@ -3,16 +3,19 @@ import SingleCardViewContainer from '../containers/SingleCardViewContainer';
 import CardsTableContainer from '../containers/CardsTableContainer';
 import AppBar from '../component-library/mui/components/AppBar';
 import ToolbarMixin from '../component-library/mui/components/ToolbarMixin';
+import Container from '../component-library/mui/components/Container';
 
 const MainView = (props) => {
   return (
     <React.Fragment>
-      <AppBar title="MTG Management" />
-      <ToolbarMixin>
-        {/* Routing Goes Here */}
-        <SingleCardViewContainer />
-        <CardsTableContainer />
-      </ToolbarMixin>
+      <AppBar title='MTG Management' />
+      <Container disableGutters={false}>
+        <ToolbarMixin>
+          {/* Routing Goes Here */}
+          <SingleCardViewContainer />
+          <CardsTableContainer />
+        </ToolbarMixin>
+      </Container>
     </React.Fragment>
   );
 };
