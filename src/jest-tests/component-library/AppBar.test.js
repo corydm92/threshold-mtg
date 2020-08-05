f9430fd3fd7eba78fd6deae50d6a6d5cfeda068e
Cory Morrissey <contact@corymorrissey.com>
2020-08-05 12:04:12 -0700

    working single test with a few examples

--- src/jest-tests/component-library/AppBar.test.js
+++ src/jest-tests/component-library/AppBar.test.js
@@ -2,4 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import AppBar from '../../js/component-library/mui/components/AppBar';
 
-it('It renders without errors', () => {});
+describe('App Bar Component', () => {
+  it('Renders AppBar without errors', () => {
+    const component = shallow(<AppBar title={'test title'} />);
+    // console.log(component.debug()); // Shows render tree
+
+    const appbar = component.find("[data-testid='appbar']");
+    const title = component.find("[data-testid='appbar-title']");
+
+    expect(appbar).toHaveLength(1); // Tests for existance
+    expect(title.text().includes('test title')).toBe(true); // Tests for value
+  });
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,8 @@
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+// File not referenced, only used for configuration
+
 Enzyme.configure({
   adapter: new EnzymeAdapter(),
   disableLifecycleMethods: true,
