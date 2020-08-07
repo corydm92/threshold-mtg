4f2f559232ba972b02a5e4650bb0491378067635
Cory Morrissey <contact@corymorrissey.com>
2020-08-07 13:26:14 -0700

    about to remove redux-test-utils

---
+++ src/jest-tests/containers/HomePageContainer.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import HomePageContainer from '../../js/containers/HomePageContainer';
+import configureStore from 'redux-mock-store';
+import { fullState } from '../../js/constants/reduxStoreMock';
+
+const configureMockStore = configureStore();
+
+const initialState = {};
+
+const store = configureMockStore(fullState);
+
+describe('HomePageContainer tests', () => {
+  it('Renders the Component', () => {
+    const component = shallow(<HomePageContainer store={store} />).dive();
+
+    expect(component).toBeTruthy(); // Tests for existance
+  });
+});
