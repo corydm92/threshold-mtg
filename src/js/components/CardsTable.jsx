f3ad55a5c68e0a9b4d9ab3e91e5c0a40c1742747
Cory Morrissey <contact@corymorrissey.com>
2020-08-19 10:01:38 -0700

    removed index from table, each table will use building blocks from the table directory in MUI instead of having one single modular table component

--- src/js/components/CardsTable.jsx
+++ src/js/components/CardsTable.jsx
@@ -1,16 +1,114 @@
 import React from 'react';
-import Table from '../component-library/mui/components/Table/';
+import TableContainer from '../component-library/mui/components/Table/TableContainer';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '../component-library/mui/components/Table/Table';
+import TableHead from '../component-library/mui/components/Table/TableHead';
+import TableBody from '../component-library/mui/components/Table/TableBody';
+import TableRow from '../component-library/mui/components/Table/TableRow';
+import TableCell from '../component-library/mui/components/Table/TableCell';
+import TablePagination from '../component-library/mui/components/Table/TablePagination';
+import Spinner from '../component-library/mui/components/Spinner';
+import Grid from '@material-ui/core/Grid';
 
-const CardsTable = (props) => {
-  const { cards, isLoadingCards } = {
-    ...props,
-  };
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
+const tableHeaders = [
+  { label: 'Card Name', colSpan: 6 },
+  { label: 'Spread', colSpan: 1 },
+  { label: 'Quantity', colSpan: 1 },
+  { label: 'Avg Purchase Price', colSpan: 2 },
+  { label: 'TCG Price', colSpan: 2 },
+];
+
+const MuiTable = (props) => {
+  const classes = useStyles();
+
+  const { cards, isLoadingCards } = { ...props };
 
   return (
-    <div data-test='cardsTable'>
-      <Table cards={cards} isLoadingCards={isLoadingCards} />
-    </div>
+    <React.Fragment>
+      <TableContainer>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <Grid container>
+                {tableHeaders.map((header) => {
+                  return (
+                    <Grid item xs={header.colSpan}>
+                      <TableCell>{header.label}</TableCell>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </TableRow>
+          </TableHead>
+          {!isLoadingCards && (
+            <TableBody>
+              {cards.map((card) => {
+                return (
+                  <TableRow>
+                    <Grid container>
+                      <Grid item xs={6}>
+                        <TableCell>
+                          <div>
+                            <div>{card.cardName}</div>
+                            <div>{card.setName}</div>
+                          </div>
+                        </TableCell>
+                      </Grid>
+                      <Grid item xs={1}>
+                        <TableCell>{card.spread}</TableCell>
+                      </Grid>
+                      <Grid item xs={1}>
+                        <TableCell>{card.quantity}</TableCell>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <TableCell>{card.avgPurchasePrice}</TableCell>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <TableCell>{card.tcgPrice}</TableCell>
+                      </Grid>
+                    </Grid>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          )}
+        </Table>
+
+        {isLoadingCards ? (
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell padding noBorder colSpan={5}>
+                  <Spinner />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        ) : (
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component='div'
+            className={classes.pagination}
+            noBorder
+            // count={rows.length}
+            // rowsPerPage={rowsPerPage}
+            // page={page}
+            // onChangePage={handleChangePage}
+            // onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        )}
+      </TableContainer>
+    </React.Fragment>
   );
 };
 
-export default CardsTable;
+export default MuiTable;
