46c8931c9ed23b757f399d8de21ed23e7c747d4c
Cory Morrissey <contact@corymorrissey.com>
2020-09-02 08:50:12 -0700

    removed makeStyles from mui button component

--- src/js/component-library/mui/components/Form/Button.jsx
+++ src/js/component-library/mui/components/Form/Button.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-  root: {},
-}));
 
 const EnhancedButton = (props) => {
   const {
