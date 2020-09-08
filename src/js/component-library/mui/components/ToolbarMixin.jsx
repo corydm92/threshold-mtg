a8ee89f38459de574d92d574437b7cf64547521b
Cory Morrissey <contact@corymorrissey.com>
2020-09-08 12:42:01 -0700

    requested changes

--- src/js/component-library/mui/components/ToolbarMixin.jsx
+++ src/js/component-library/mui/components/ToolbarMixin.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => {
-  console.log(theme);
   return {
     offset: theme.mixins.toolbar,
   };
