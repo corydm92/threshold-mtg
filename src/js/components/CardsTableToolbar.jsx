f2e8550949170fff7f6e9481897cf142ccef8e14
Cory Morrissey <corydm92@gmail.com>
2020-09-14 12:41:49 -0700

    created CardsTableToolbar and CardsTableMobile

---
+++ src/js/components/CardsTableToolbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import EnhancedToolbar from '../component-library/mui/components/Toolbar';
+import Grid from '@material-ui/core/Grid';
+import TableDisplayIcons from './TableDisplayIcons';
+
+const CardsTableToolbar = (props) => {
+  const { handleDisplayChange, activeDisplay } = { ...props };
+  return (
+    <EnhancedToolbar>
+      <Grid container>
+        <Grid item xs={10}>
+          {/* Left Aligned Items Here */}
+        </Grid>
+        <Grid item xs={2}>
+          <TableDisplayIcons
+            onClick={handleDisplayChange}
+            activeDisplay={activeDisplay}
+          />
+        </Grid>
+      </Grid>
+    </EnhancedToolbar>
+  );
+};
+
+export default CardsTableToolbar;
